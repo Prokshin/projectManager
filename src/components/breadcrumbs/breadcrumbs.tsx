0e1d0d5e5c8d55e688b04295aa1af07b5bf0c752
Prokshin <43705427+Prokshin@users.noreply.github.com>
2020-03-04 16:22:34 +0500

    refactoring breadcrumbs to typescript

--- src/components/breadcrumbs/breadcrumbs.tsx
+++ src/components/breadcrumbs/breadcrumbs.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "./breadcrumbs.css";
+
+interface BreadcrumbsProps {
+  links: [
+    {
+      url: string;
+      name: string;
+    }
+  ];
+}
+
 //! Продумать получение данных о родителях
-const Breadcrumbs = props => {
+const Breadcrumbs: React.FC<BreadcrumbsProps> = props => {
   let first = true;
   const links = props.links.map(el => {
-    let arrow = "";
+    let arrow = <></>;
     if (first) {
       first = !first;
     } else {
